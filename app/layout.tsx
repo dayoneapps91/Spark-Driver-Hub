bfe06d12f7071d78420f723e53c142172a7baeaf
dayoneapps91 <dayoneapps91@gmail.com>
2026-06-30 23:13:14 -0600

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./globals.css";
@@ -53,6 +54,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0CN2RDZCXD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0CN2RDZCXD');
+          `}
+        </Script>
+
         <Header />
         {children}
         <Footer />
